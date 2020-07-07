0bfa93e76fcc417fc399473f443a5c96d8e4ed15
ptaylor <ptaylor@aimconsulting.com>
2020-07-07 13:39:41 -0700

    changed dictionary lookup method to return none instead of throwing a keyerror

--- features/mobileOrders_steps.py
+++ features/mobileOrders_steps.py
@@ -10,8 +10,9 @@ def set_user_store(self):
 
 @step(r'a "([^"]*)" is ordered')
 def order_beverage(self, ordered_beverage):
-    world.actual_beverage = StoreAdapter.store_menu[ordered_beverage]
+    world.actual_beverage = StoreAdapter.store_menu.get(ordered_beverage)
+    assert_is_not_none(world.actual_beverage, "{beverage_name} is not found on the menu".format(beverage_name = ordered_beverage))
 
 @step(r'the selected store prints a label with the text "([^"]*)"')
 def validate_label(self, expected_label_text):
-    assert_equal(world.actual_beverage['label_text'], expected_label_text)
+    assert_equal(world.actual_beverage['label_text'], expected_label_text, "the expected label text of {expected_value} was not on the label. Instead the label text found was {actual_value}".format(expected_value = expected_label_text, actual_value = world.actual_beverage["label_text"]))
