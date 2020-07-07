8c0670435129f6658cf79f438a2bd3b684af70fc
ptaylor <ptaylor@aimconsulting.com>
2020-07-07 13:12:23 -0700

    Update Latte label import the store Adapter for use as business logic stub

--- features/StoreAdapter.py
+++ features/StoreAdapter.py
@@ -1,5 +1,5 @@
 store_menu = {
     "coffee": {"price":1.00, "label_text":"brew"},
-    "latte": {"price":1.50, "label_text":"Cafe Au Lait"}, 
+    "latte": {"price":1.50, "label_text":"Cafe Latte"}, 
     "mocha": {"price":1.75, "label_text":"Cafe Mocha"}  
     }
--- features/mobileOrders_steps.py
+++ features/mobileOrders_steps.py
@@ -1,13 +1,8 @@
 from aloe import before, step, world
 from asserts import *
+from features import StoreAdapter
 
-@before.each_example
-def setupStore(*args):
-    world.menu = {
-    "coffee": {"price":1.00, "label_text":"brew"},
-    "latte": {"price":1.50, "label_text":"Cafe Au Lait"}, 
-    "mocha": {"price":1.75, "label_text":"Cafe Mocha"}  
-    }
+
 
 @step(r'a User on mobile with a selected store')
 def set_user_store(self):
@@ -15,7 +10,7 @@ def set_user_store(self):
 
 @step(r'a "([^"]*)" is ordered')
 def order_beverage(self, ordered_beverage):
-    world.actual_beverage = world.menu[ordered_beverage]
+    world.actual_beverage = StoreAdapter.store_menu[ordered_beverage]
 
 @step(r'the selected store prints a label with the text "([^"]*)"')
 def validate_label(self, expected_label_text):
